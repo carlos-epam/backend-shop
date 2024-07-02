1523f96a8a5d5aace2f0d94d6cb26a7ae3955835
carlos-epam <carlos_delacruz@epam.com>
2024-07-02 12:02:47 -0600

    wip

--- catalogBatchProcess.js
+++ catalogBatchProcess.js
@@ -1,15 +1,20 @@
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
 const { v4: uuidv4 } = require('uuid');
 
 const ddbClient = new DynamoDBClient();
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+const snsClient = new SNSClient();
 
 const productsTable = "products";
 const stockTable = "stock";
+const snsArn = process.env.SNS_ARN;
 
 module.exports.handler = async (event) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
+
+  const createdProducts = [];
 
   for (const record of event.Records) {
     const messageBody = JSON.parse(record.body);
@@ -49,9 +54,27 @@ module.exports.handler = async (event) => {
 
       await ddbDocClient.send(new PutCommand(stockParams));
 
+      createdProducts.push(newProduct);
       console.log('Successfully created product:', newProduct.id);
     } catch (error) {
       console.error('Error processing message:', error);
+    }
+  }
+
+  if (createdProducts.length > 0) {
+    try {
+      const snsParams = {
+        Message: JSON.stringify({
+          message: `Successfully created ${createdProducts.length} products`,
+          products: createdProducts
+        }),
+        TopicArn: snsArn
+      };
+
+      await snsClient.send(new PublishCommand(snsParams));
+      console.log('Sent  notification');
+    } catch (error) {
+      console.error('Error sending SNS notification:', error);
     }
   }
 
